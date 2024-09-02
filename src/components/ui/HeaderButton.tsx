abc73e780b267bceb6b22c240fdfa64c35bb235a
KrishnaTripathy <krishnatripathy2001@gmail.com>
2024-09-02 16:25:59 -0400

    done

--- src/components/ui/HeaderButton.tsx
+++ src/components/ui/HeaderButton.tsx
@@ -1,18 +1,26 @@
-import Image from "next/image"
-type ButtonProps = {
-    type: 'button' | 'submit';
-    title: string;
-    icon?: string;
-    variant: string;
-}
+import Image from "next/image";
+import { ReactNode } from "react";
 
-const HeaderButton = ({type,title,icon,variant}:ButtonProps) => {
+type ButtonProps = {
+  type: 'button' | 'submit';
+  title: string;
+  icon?: ReactNode; // Change to ReactNode to support components
+  variant: string;
+};
+
+const HeaderButton = ({ type, title, icon, variant }: ButtonProps) => {
   return (
     <button className={`flexCenter gap-3 rounded-full border ${variant}`} type={type}>
-     {icon && <Image src={icon} alt={title} width={24} height={24}/>}
-     <label className="bold-16 whitespace-nowrap cursor-pointer">{title}</label>
+      {icon && (
+        typeof icon === "string" ? (
+          <Image src={icon} alt={title} width={24} height={24} />
+        ) : (
+          icon // Render icon component if it's not a string
+        )
+      )}
+      <label className="bold-16 whitespace-nowrap cursor-pointer">{title}</label>
     </button>
-  )
-}
+  );
+};
 
 export default HeaderButton;
